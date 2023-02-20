49722c2faf3f723ad9230917cd9464d7ab8f4679
Caitlin <2423179@dundee.ac.uk>
2023-02-20 22:48:37 +0000

    Markdown file with credits, working menu buttons, fade in and out of scenes.

---
+++ Assets/Resources/credits.md
@@ -0,0 +1,18 @@
+<h1>Credits
+
+<h2>Sprites
+<ul>
+<li>Wolf Sprites - https://finalbossblues.itch.io/wolf-pack</li>
+</ul>
+
+---
+<h2>Music
+<ul>
+<li>Forest Lullaby - https://pixabay.com/music/acoustic-group-forest-lullaby-110624/</li>
+<li>Walk Together - https://pixabay.com/music/acoustic-group-walk-together-123281/</li>
+</ul>
+
+---
+<h2>Icons
+<ul>
+<li>Font Awesome Icons - https://fontawesome.com/
--- Assets/Scripts/change_scene.cs
+++ Assets/Scripts/change_scene.cs
@@ -5,18 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class change_scene : MonoBehaviour
 {
-    // A function to load the launch screen
-    public void load_launch_screen() {
-        SceneManager.LoadScene("Launch_Screen");
-    }
+
+    // instance of animator
+    public Animator animator;
 
     // A function to close the game entirely
-    public void shut_down_game() {
+    private void shut_down_game() {
         Application.Quit();
     }
 
-    // a function to show the credits
-    public void show_credits() {
-        SceneManager.LoadScene("Credits");
+    // a function to load the scenes.
+    // takes in the name of the scene to be loaded as a string
+     IEnumerator loadSceneTransitions(string scene) {
+        // Calls FadeOut which triggers fade out
+        animator.SetTrigger("FadeOut");
+        // Waits a second
+        yield return new WaitForSeconds(1f);
+        // Loads scene
+        SceneManager.LoadScene(scene);
+
+        // fades_in
+        animator.SetTrigger("FadeIn");
     }
+
+    // Loads scene
+    public void loadScene(string sceneToLoad) {
+        // Starts a Coroutine (basically multithreading)
+        StartCoroutine(loadSceneTransitions(sceneToLoad));
+    }
+
 }
