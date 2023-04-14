c81e580ce4f6d13bcaecba59e741aafb17c446bd
Cait <2423179@dundee.ac.uk>
2023-04-14 18:52:36 +0100

    there is still a infinite loop and minor issues but generally is working. Added commentary.

--- Assets/Scripts/Level Generation/LevelGen.cs
+++ Assets/Scripts/Level Generation/LevelGen.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class LevelGen : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class LevelGen : MonoBehaviour
 
     public GameObject[] endLayouts; //all rooms that only have one exit/entrance
 
-    private int previousRoom; // the previous room generated
+    private int previousRoom { get; set; } = -1; // the previous room generated
 
     private bool firstRun { get; set; } = true; //if first run of generation
 
@@ -23,49 +24,53 @@ public class LevelGen : MonoBehaviour
     // A function to generate the start location
     public void generateStartLocation()
     {
-        // Stores the current poisition in list
+        // Stores the current position in list
         int index = Random.Range(0, gc.rooms.Count);
-        print("START INDEX HERE: " + index);
+       
         // Gets random start from the list of potential start locations
         GameObject startLoc = gc.rooms[index];
 
+        //Calls newRoom
         newRoom(startLoc, index);
     }
 
     //A function to create a new room
-    // nl = the room to be replaced
-    // index = position in the list of rooms
+    //@param nl = the room to be replaced
+    //@param index = position in the list of rooms
     private void newRoom(GameObject nl, int index)
     {
         GameObject layout = generateRoomLayout();
 
-        // While it doesn't fit the conditions
         while (
-            placementValidation(layout, index) == false || checkPreviousRoom(layout, index) == false
+            gridPlacementValidation(layout, index) == false
+            || checkSurroundingRoom(layout, index) == false
+            || checkPreviousRoom(layout, index) == false
         )
         {
-            // If length of tag = 1, means it is one of the rooms with only one exit
-            if (
-                firstRun == true
-                && placementValidation(layout, index) == true
-                && layout.tag.Length != 1
-            )
+            //If it is on its first run (and therefore doesn't have a previous room)
+            if (firstRun == true && gridPlacementValidation(layout, index) == true )
             {
                 firstRun = false;
                 break;
             }
-            else if (firstRun == false)
+
+            //if first run is false and meets conditions, breaks from loop
+            else if (firstRun == false && gridPlacementValidation(layout, index) == true
+            && checkSurroundingRoom(layout, index) == true
+            && checkPreviousRoom(layout, index) == true)
             {
-                layout = generateRoomLayout();
-                print("not first run");
-                continue;
+                break;
             }
+
+            //else, sets a new layout, and continues
             else
             {
                 layout = generateRoomLayout();
+                continue;
             }
         }
 
+        //Instantiates new room as room
         GameObject room = Instantiate(
             layout,
             nl.transform.position,
@@ -73,35 +78,46 @@ public class LevelGen : MonoBehaviour
             nl.transform.parent
         );
 
+        
         Destroy(nl); //removes old room
-        room.name = index.ToString();
-        gc.rooms[index] = room;
-        previousRoom = index;
+        room.name = index.ToString(); //sets the name of the room to the index
+        gc.rooms[index] = room; //replaces room in list with new room
+        previousRoom = index; //previous room is set to index
     }
 
     // A function to create a new end room;
     //@param: index - position in the list of rooms
     private void newRoom(int index)
     {
-        print("Last Room: " + index);
+
         GameObject layout = generateEndRoomLayout();
 
-        // While it doesn't fit the conditions
         while (
-            placementValidation(layout, index) == false || checkPreviousRoom(layout, index) == false
+            gridPlacementValidation(layout, index) == false
+            || checkSurroundingRoom(layout, index) == false
+            || checkPreviousRoom(layout, index) == false
         )
         {
-            // If length of tag = 1, means it is one of the rooms with only one exit
-            if (
-                placementValidation(layout, index) == true
-                && checkPreviousRoom(layout, index) == true
-            )
+            //If it is on its first run (and therefore doesn't have a previous room)
+            if (firstRun == true && gridPlacementValidation(layout, index) == true )
+            {
+                firstRun = false;
+                break;
+            }
+
+            //if first run is false and meets conditions, breaks from loop
+            else if (firstRun == false && gridPlacementValidation(layout, index) == true
+            && checkSurroundingRoom(layout, index) == true
+            && checkPreviousRoom(layout, index) == true)
             {
                 break;
             }
+
+            //else, sets a new layout, and continues
             else
             {
                 layout = generateEndRoomLayout();
+                continue;
             }
         }
 
@@ -113,9 +129,9 @@ public class LevelGen : MonoBehaviour
         );
 
         Destroy(gc.rooms[index]); //removes old room
-        room.name = index.ToString();
-        gc.rooms[index] = room;
-        previousRoom = index;
+        room.name = index.ToString(); //sets name of room to index
+        gc.rooms[index] = room; //replaces room in list with new room
+        previousRoom = index; //previousRoom is set to index
     }
 
     //Generates the normal room layout
@@ -135,43 +151,88 @@ public class LevelGen : MonoBehaviour
     {
         for (int i = 0; i <= mainPathLength; i++)
         {
-            print(i);
             GameObject prevRoom = gc.rooms[previousRoom];
 
-            List<char> banned = new List<char>();
+            List<char> banned = new List<char>(); //list of banned directions
 
             // random direction
             char dir = prevRoom.tag[Random.Range(0, prevRoom.tag.Length)];
 
-            int index;
+            int index; //temp index
 
             while (dir != -1)
             {
                 try
                 {
+                    //if direction is up and banned does not contain U
                     if (dir == 'U' && !banned.Contains('U'))
                     {
+                        //set index to index minus ten
                         index = previousRoom - 10;
-                        newRoomSwitch(i, index);
-                        break;
+
+                        //if empty then call newRoomswitch
+                        if (checkEmpty(index) == true)
+                        {
+                            newRoomSwitch(i, index);
+                            break;
+                        }
+                        //if not throw exception
+                        else
+                        {
+                            throw new System.Exception();
+                        }
                     }
+                    //if direction is down and banned does not contain D
                     else if (dir == 'D' && !banned.Contains('D'))
                     {
+                        //set index to index plus ten
                         index = previousRoom + 10;
-                        newRoomSwitch(i, index);
-                        break;
+                       //if empty then call newRoomswitch
+                        if (checkEmpty(index) == true)
+                        {
+                            newRoomSwitch(i, index);
+                            break;
+                        }
+                        //if not throw exception
+                        else
+                        {
+                            throw new System.Exception();
+                        }
                     }
+                    //if direction is L and banned does not contain L
                     else if (dir == 'L' && !banned.Contains('L'))
-                    {
+                    {   
+                        //subtract one from index
                         index = previousRoom - 1;
-                        newRoomSwitch(i, index);
-                        break;
+                       //if empty then call newRoomswitch
+                        if (checkEmpty(index) == true)
+                        {
+                            newRoomSwitch(i, index);
+                            break;
+                        }
+                        //if not throw exception
+                        else
+                        {
+                            throw new System.Exception();
+                        }
                     }
+
+                    //if direction is R and banned does not contain R
                     else if (dir == 'R' && !banned.Contains('R'))
                     {
+                        //add one to index
                         index = previousRoom + 1;
-                        newRoomSwitch(i, index);
-                        break;
+                        //if empty then call newRoomswitch
+                        if (checkEmpty(index) == true)
+                        {
+                            newRoomSwitch(i, index);
+                            break;
+                        }
+                        //if not throw exception
+                        else
+                        {
+                            throw new System.Exception();
+                        }
                     }
                     else
                     {
@@ -183,7 +244,14 @@ public class LevelGen : MonoBehaviour
                 {
                     print("Out of the Grid. Trying another direction...");
                     banned.Add(dir);
-                    dir = prevRoom.tag[Random.Range(0, prevRoom.tag.Length)];
+                    if (banned.Count == 4)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dir = prevRoom.tag[Random.Range(0, prevRoom.tag.Length)];
+                    }
                 }
             }
         }
@@ -209,8 +277,9 @@ public class LevelGen : MonoBehaviour
     //validate placement
     //@param: layout - the room layout
     //@param: ind - index of the current room
-    private bool placementValidation(GameObject layout, int ind)
+    private bool gridPlacementValidation(GameObject layout, int ind)
     {
+        DEBUG("GridPlacement", layout, ind);
         //if index is certain numbers, cannot be certain layouts.
         if (ind == 0)
         {
@@ -288,9 +357,15 @@ public class LevelGen : MonoBehaviour
     //@param: ind - index of the current room
     private bool checkPreviousRoom(GameObject layout, int ind)
     {
+        DEBUG("prevRooms", layout, ind);
         // if the previous room doesn't have the corresponding direction
         //so if previous room doesn't have left in its tag, and the new room has right, recalculate
-        if (
+        //will only be -1 on first run
+        if (previousRoom == -1)
+        {
+            return true;
+        }
+        else if (
             layout.tag.Contains("L")
             && gc.rooms[previousRoom].tag.Contains("R")
             && ind == previousRoom + 1
@@ -332,4 +407,86 @@ public class LevelGen : MonoBehaviour
             return false;
         }
     }
+
+    //Checks the surrounding rooms to make sure paths connect
+    //@param: layout - the room layout
+    //@param: ind - index of the current room
+    private bool checkSurroundingRoom(GameObject layout, int ind)
+    {
+        DEBUG("SurroundingRooms", layout, ind);
+
+        //checks room above
+        if (
+            ind > 9
+            && layout.tag.Contains("U")
+            && !(gc.rooms[ind - 10].tag.Contains("D") || gc.rooms[ind - 10].tag.Contains("ER"))
+        )
+        {
+            DEBUG("first", gc.rooms[ind - 10], -1);
+            return false;
+        }
+        //checks room to the left
+        else if (
+            !ind.ToString().Contains("0")
+            && layout.tag.Contains("L")
+            && !(gc.rooms[ind - 1].tag.Contains("R") || gc.rooms[ind - 1].tag.Contains("E"))
+        )
+        {
+            DEBUG("second", gc.rooms[ind - 10], -2);
+            return false;
+        }
+        //checks room to the right
+        else if (
+            !ind.ToString().Contains("9")
+            && layout.tag.Contains("R")
+            && !(gc.rooms[ind + 1].tag.Contains("L") || gc.rooms[ind + 1].tag.Contains("E"))
+        )
+        {
+            DEBUG("third", gc.rooms[ind - 10], -3);
+            return false;
+        }
+        //checks room below
+        else if (
+            ind < 90
+            && layout.tag.Contains("D")
+            && !(gc.rooms[ind + 10].tag.Contains("U") || gc.rooms[ind + 10].tag.Contains("E"))
+        )
+        {
+            DEBUG("fourth", gc.rooms[ind - 10], -4);
+            return false;
+        }
+
+        return true;
+    }
+
+
+    //debug function
+    //to be removed in future as i borrowed it and haven't cited it
+    //if i forget, really sorry, this isn't mine and should have been removed.
+    private void DEBUG(string func, GameObject stuff, int ind)
+    {
+        string path = "Debug/Infi.txt";
+
+        //Write some text to the test.txt file
+
+        StreamWriter writer = new StreamWriter(path, true);
+
+        writer.WriteLine(func + " " + stuff.tag + " " + ind);
+
+        writer.Close();
+    }
+
+    private bool checkEmpty(int index)
+    {
+        if (gc.rooms[index].tag.Contains("ER"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
+//no zero for left,
+//no 9 for right
