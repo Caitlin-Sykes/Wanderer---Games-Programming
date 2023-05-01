9ec795ae57cfdecfb88a22ccbfe33ba2a01c4c13
Cait <2423179@dundee.ac.uk>
2023-05-01 17:43:14 +0100

    Camera works!

--- Assets/Scripts/Level Generation/Doors.cs
+++ Assets/Scripts/Level Generation/Doors.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private LevelNav ln;
+
+    public enum Clear
+    {
+        Cleared,
+        NotCleared
+    };
+
+    public Clear clear = Clear.Cleared;
+
     void Start()
     {
-        
+        ln = Camera.main.GetComponent<LevelNav>();
+        print(ln);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collide)
     {
-        
+        int index;
+        print("tagi" + this.tag);
+
+        if (collide.tag == "Player")
+        {
+            //S1: Up, S2: Right, S3: Down, S4:Left
+            switch (this.tag)
+            {
+                case "S1":
+                    index = Int32.Parse(this.transform.parent.name);
+                    print("b4 " + index);
+                    ln.MoveCamera((index - 10), "S1");
+                    break;
+
+                case "S2":
+                    index = Int32.Parse(this.transform.parent.name);
+                    print("b4 " + index);
+                    ln.MoveCamera((index + 1), "S2");
+                    break;
+                case "S3":
+                    index = Int32.Parse(this.transform.parent.name);
+                    print("b4 " + index);
+                    Destroy(GameObject.Find("Player"));
+                    ln.MoveCamera((index + 10), "S3");
+                    break;
+                case "S4":
+                    index = Int32.Parse(this.transform.parent.name);
+                    print("b4 " + index);
+                    ln.MoveCamera((index - 1), "S4");
+                    break;
+
+                default:
+                    print("sus");
+                    break;
+            }
+        }
     }
 }
--- Assets/Scripts/Level Generation/LevelNav.cs
+++ Assets/Scripts/Level Generation/LevelNav.cs
@@ -11,13 +11,15 @@ public class LevelNav : MonoBehaviour
 
     private GameObject currentRoom {get; set;}
 
+    public GameObject player;
+
     void Awake() {
         cam = this.GetComponent<Camera>();
     }
 
+    //A function to generate the starting room
     public void startPlayerRoom() {
         currentRoom = gc.rooms[lg.randomRoom()];
-        // print(startRoom);
     }
 
     //A function to initialise the starting camera
@@ -25,13 +27,46 @@ public class LevelNav : MonoBehaviour
 
         Transform pos = currentRoom.transform.Find("SpawnInit");
         cam.transform.localPosition = Vector3.Lerp(transform.position, pos.position, 1); 
+
+        Instantiate(player, pos.position, Quaternion.identity); 
+
     }
 
     //A function to move the camera to the next room
-    private void MoveCamera(int dir) {
+    //@param roomInt - index of the new room
+    //@param dir - direction of the door
+    public void MoveCamera(int roomInt, string dir) {
+        
+        //Sets current room to new room
+        currentRoom = gc.rooms[roomInt];
 
-        // Transform pos = room.transform.Find("SpawnInit");
-        // pos.position -= new Vector3(0,0,1);
-        // cam.transform.localPosition = Vector3.Lerp(transform.position, pos.position, 1); 
+        Transform pos;
+        
+        //If S1, need the down spawn position
+        if (dir == "S1") {
+            pos = currentRoom.transform.Find("SpawnD");
+        }
+        //If S2, need the left spawn position
+        else if (dir == "S2") {
+            pos = currentRoom.transform.Find("SpawnL");
+        }
+
+        //If S3, need the up spawn position
+        else if (dir == "S3") {
+            pos = currentRoom.transform.Find("SpawnU");
+        }
+
+        else {
+            pos = currentRoom.transform.Find("SpawnR");
+
+        }
+       
+        //Destroys previous player object
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        //Sets camera to the centre of the room
+        cam.transform.localPosition = Vector3.Lerp(transform.position, currentRoom.transform.Find("SpawnInit").position, 1); 
+
+        //Instantiate new player
+        Instantiate(player, pos.position, Quaternion.identity); 
     }
 }
