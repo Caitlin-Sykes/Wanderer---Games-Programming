f974d58828094b3c61fa179ab67d4d527723a92d
Cait <2423179@dundee.ac.uk>
2023-05-01 13:46:18 +0100

    fixed prefabs added collisions

--- Assets/Scripts/Level Generation/LevelNav.cs
+++ Assets/Scripts/Level Generation/LevelNav.cs
@@ -9,30 +9,29 @@ public class LevelNav : MonoBehaviour
 
     public GridController gc;
 
-    private GameObject startRoom {get; set;}
+    private GameObject currentRoom {get; set;}
 
     void Awake() {
         cam = this.GetComponent<Camera>();
     }
 
     public void startPlayerRoom() {
-        startRoom = gc.rooms[lg.randomRoom()];
-        print(startRoom);
+        currentRoom = gc.rooms[lg.randomRoom()];
+        // print(startRoom);
     }
 
     //A function to initialise the starting camera
     public void MoveCamera() {
 
-        Transform pos = startRoom.transform.Find("SpawnInit");
-        // pos.position -= new Vector3(0,0,1);
+        Transform pos = currentRoom.transform.Find("SpawnInit");
         cam.transform.localPosition = Vector3.Lerp(transform.position, pos.position, 1); 
     }
 
     //A function to move the camera to the next room
-    private void MoveCamera(GameObject room) {
+    private void MoveCamera(int dir) {
 
-        Transform pos = room.transform.Find("SpawnInit");
+        // Transform pos = room.transform.Find("SpawnInit");
         // pos.position -= new Vector3(0,0,1);
-        cam.transform.localPosition = Vector3.Lerp(transform.position, pos.position, 1); 
+        // cam.transform.localPosition = Vector3.Lerp(transform.position, pos.position, 1); 
     }
 }
